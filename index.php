99f72070fae92da63299b63a04bed6377042f696
Yannick <yannick.huet@student.graafschapcollege.nl>
2026-04-26 23:28:10 +0200

    iets aangepast aan de header

--- index.php
+++ index.php
@@ -266,7 +266,7 @@ if (isset($_POST['submit'])) {
         </form>
     </section>
     <footer>
-        <p>Copyright &copy; 2026 Yannick van Huet | All Rights, Reserved</p>
+        <p>Copyright &copy; 2026 Yannick van Huet | All Rights Reserved</p>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
